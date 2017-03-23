2d0fbb6c825aa06e425faf1c76db30c00aeb0090
Mathias Stocker <mathias.stocker@students.unibe.ch>
2017-03-24 00:30:52 +0100

    Add main logic for edit/post data

--- wordpress/wp-content/plugins/bergclub-plugin/Adressverwaltung/Controllers/MainController.php
+++ wordpress/wp-content/plugins/bergclub-plugin/Adressverwaltung/Controllers/MainController.php
@@ -13,51 +13,82 @@ class MainController extends AbstractController
     protected $view = 'pages.main';
 
     protected function first(){
-        if(isset($_GET['action']) && $_GET['action'] == 'delete'){
-            User::remove($_GET['id']);
-            FlashMessage::add(FlashMessage::TYPE_SUCCESS, 'Datensatz erfolgreich gelöscht.');
-            Helpers::redirect('?page=' . $_GET['page']);
-        }
+        $view = $this->getGET('view', 'main');
 
-        if(isset($_GET['view'])){
-            $view = $_GET['view'];
-            if($view == "detail"){
-                $this->data['edit'] = false;
-                $tab = "data";
-                if(isset($_GET['tab'])){
-                    $tab = $_GET['tab'];
-                }
-                $viewtype = "show";
-                //TODO: check rights
-                if(isset($_GET["edit"])){
-                    $this->data['edit'] = true;
-                    $viewtype = "edit";
-                }
-
-                $this->data['tab'] = $tab;
-                $this->data['tab_file'] = 'includes.' . $view . '-' . $tab . '-' . $viewtype;
-            }
-            $this->view = 'pages.' . $view;
-        }
+        $this->view = 'pages.' . $view;
 
         if($this->view == 'pages.main') {
             $this->data['title'] = "Adressverwaltung";
             $this->data['users'] = User::findAll();
+
         }elseif($this->view == 'pages.detail'){
+            $this->prepareInclude();
+
             $user = $this->data['user'] = User::find($_GET['id']);
+
             if($user) {
                 $this->data['title'] = $user->last_name . ' ' . $user->first_name;
+
             }else{
-                $this->data['title'] = "Datensatz nicht gefunden";
                 FlashMessage::add(FlashMessage::TYPE_ERROR, 'Der gewünschte Datensatz existiert nicht.');
+                $this->view = 'pages.empty';
             }
         }
     }
 
-    protected function get(){}
+    protected function get(){
+        $action = $this->getGET('action', null);
+        if($action == 'delete'){
+            User::remove($_GET['id']);
+            FlashMessage::add(FlashMessage::TYPE_SUCCESS, 'Datensatz erfolgreich gelöscht.');
+            Helpers::redirect('?page=' . $_GET['page']);
+        }
+    }
 
-    protected function post(){}
+    protected function post(){
+        $method = "post" . strtoupper(substr($this->data['tab'], 0, 1)) . substr($this->data['tab'], 1);
+        if(method_exists($this, $method)){
+            $this->$method();
+        }
+    }
 
-    protected function last(){}
+    protected function last(){
 
+    }
+
+
+    private function postData(){
+        FlashMessage::add(FlashMessage::TYPE_INFO, 'Benutzerdaten wurden geposted.');
+        //HIER DEN FORMULARPOST DER BENUTZERDATEN VERWARBEITEN
+    }
+
+    private function postFunctions(){
+        FlashMessage::add(FlashMessage::TYPE_INFO, 'Funktionsrollen wurden geposted.');
+        //HIER DEN FORMULARPOST DER FUNKTIONSROLLEN VERARBEITEN
+    }
+
+    private function postHistory(){
+        FlashMessage::add(FlashMessage::TYPE_INFO, 'Historie wurde geposted.');
+        //HIER DEN FORMULARPOST DER HISTORIE VERWARBEITEN
+    }
+
+
+
+    private function prepareInclude(){
+        $this->data['tab'] = $this->getGET('tab', 'data');
+        $this->data['edit'] = $this->getGET('edit', 0);
+        $viewType = "show";
+        if($this->data['edit']){
+            $viewType = "edit";
+        }
+
+        $this->data['tab_file'] = str_replace('pages.', 'includes.', $this->view) . '-' . $this->data['tab'] . '-' . $viewType;
+    }
+
+    private function getGET($key, $default){
+        if(isset($_GET[$key])){
+            return $_GET[$key];
+        }
+        return $default;
+    }
 }
