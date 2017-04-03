60c80c54359aa32f1696bfabf664414849a486ff
Mathias Stocker <mathias.stocker@students.unibe.ch>
2017-04-03 12:47:56 +0200

    Remove commented code lines in js

--- wordpress/wp-content/themes/bergclub-theme/js/contact-form-functions.js
+++ wordpress/wp-content/themes/bergclub-theme/js/contact-form-functions.js
@@ -1,33 +1,3 @@
-/*
-$(document).ready(function(){
-
-    $(".birthday").hide();
-
-    var $inputs = $('input[name=phone-p], input[name=phone-g], input[name=phone-m], input[name=email]');
-    $inputs.on('input', function() {
-        var val = $('#enquirytype').val();
-        if(val==="membership") {
-            $inputs.not(this).prop('required', !$(this).val().length);
-        }
-    } );
-
-    $("#enquirytype").change(function(){
-        $('.maybe-required').prop('required',false);
-
-        var val = $('#enquirytype').val();
-        $('.' + val + '-required').prop('required', true);
-        $('.always-required').prop('required', true);
-
-        if(val==="membership"){
-            $('input[name=email]').prop('required', true);
-            $(".birthday").show();
-        } else {
-            $(".birthday").hide();
-        }
-    })
-;});
-*/
-
 $("#enquirytype").change(function(){
     updateFields();
 });
