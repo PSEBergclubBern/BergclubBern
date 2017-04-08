b9e09ebff27fec1df0f62bed5a468b3ba85f3717
Timm <you@example.com>
2017-04-08 16:00:21 +0200

    some minor code cleanups

--- wordpress/wp-content/plugins/bergclub-plugin/Tourenberichte/MetaBoxes/CommonTourenberichte.php
+++ wordpress/wp-content/plugins/bergclub-plugin/Tourenberichte/MetaBoxes/CommonTourenberichte.php
@@ -12,23 +12,11 @@ namespace BergclubPlugin\Tourenberichte\MetaBoxes;
 use BergclubPlugin\FlashMessage;
 
 class CommonTourenberichte extends MetaBox {
-	const DATE_FROM_IDENTIFIER = '_dateFrom';
-	const DATE_TO_IDENTIFIER = '_dateTo';
 	const LEADER = '_leader';
-	const CO_LEADER = '_coLeader';
-	const SIGNUP_UNTIL = '_signupUntil';
-	const SIGNUP_TO = '_signupTo';
-	const SLEEPOVER = '_sleepOver';
 
 	public function getUniqueFieldNames() {
 		return array(
-			self::DATE_FROM_IDENTIFIER,
-			self::DATE_TO_IDENTIFIER,
 			self::LEADER,
-			self::CO_LEADER,
-			self::SIGNUP_UNTIL,
-			self::SIGNUP_TO,
-			self::SLEEPOVER,
 		);
 	}
 
@@ -56,19 +44,6 @@ class CommonTourenberichte extends MetaBox {
         );
         $posts_array = get_posts( $args );
 
-		$roles = wp_get_current_user()->roles;
-		if ( in_array( 'bcb_leiter', $roles ) ) {
-			$leiter = array( wp_get_current_user() );
-		} else {
-			$leiter = get_users( array( 'role' => 'bcb_leiter' ) );
-		}
-
-		/*return array(
-			'leiter'   => $leiter,
-			'coLeiter' => get_users(),
-			'signUpTo' => get_users(),
-		);*/
-
 		return array(
 			'leiter'   => $posts_array,
 			'coLeiter' => get_users(),
@@ -86,52 +61,12 @@ class CommonTourenberichte extends MetaBox {
 
 	public function isValid($values, $posttype) {
 		$errors = array();
-		if ( array_key_exists( self::DATE_FROM_IDENTIFIER, $values ) ) {
-			$date_from = \DateTime::createFromFormat( "d.m.Y", $values[ self::DATE_FROM_IDENTIFIER ] );
-			if ( $date_from === false ) {
-				$errors[] = '"Datum von" ist ungültig';
+		if ( array_key_exists( self::LEADER, $values ) ) {
+            $chosenLeader = $values[self::LEADER];
+			if ( $chosenLeader == "" ) {
+				$errors[] = 'Tourenbericht muss sich auf eine publizierte Tour beziehen.';
 			}
 		}
-
-        if ($posttype != "draft") {
-            if (array_key_exists(self::DATE_TO_IDENTIFIER, $values) && !empty($values[self::DATE_TO_IDENTIFIER])) {
-                $date_to = \DateTime::createFromFormat("d.m.Y", $values[self::DATE_TO_IDENTIFIER]);
-                if ($date_to === false) {
-                    $errors[] = '"Datum bis" ist ungültig';
-                } else {
-                    if (array_key_exists(self::DATE_FROM_IDENTIFIER, $values)) {
-                        $date_from = \DateTime::createFromFormat("d.m.Y", $values[self::DATE_FROM_IDENTIFIER]);
-                        if ($date_to < $date_from) {
-                            $errors[] = '"Datum bis" muss nach "Datum von" sein.';
-                        } elseif ($date_to > $date_from) {
-                            if (!array_key_exists(self::SLEEPOVER, $values) || $values[self::SLEEPOVER] != "Ja") {
-                                $errors[] = 'Mehrtägige Touren müssen eine Übernachtung beinhalten';
-                            }
-                        } elseif ($date_to = $date_from) {
-                            if (!array_key_exists(self::SLEEPOVER, $values) || $values[self::SLEEPOVER] != "Nein") {
-                                $errors[] = 'Eintägige Touren dürfen keine Übernachtung beinhalten';
-                            }
-                        }
-                    }
-                }
-            }
-
-            //Test SIGNUP_UNTIL valid
-            if (array_key_exists(self::SIGNUP_UNTIL, $values) && !empty($values[self::SIGNUP_UNTIL])) {
-                $date_signup = \DateTime::createFromFormat("d.m.Y", $values[self::SIGNUP_UNTIL]);
-                if ($date_signup === false) {
-                    $errors[] = '"Anmelden bis" ist kein gültiges Datum';
-                } else {
-                    if (array_key_exists(self::DATE_FROM_IDENTIFIER, $values)) {
-                        $date_from = \DateTime::createFromFormat("d.m.Y", $values[self::DATE_FROM_IDENTIFIER]);
-                        if ($date_from < $date_signup) {
-                            $errors[] = 'Die Anmeldefrist muss vor dem Start der Tour beendet sein.';
-                        }
-                    }
-                }
-            }
-        }
-
 
 		foreach ( $errors as $errorMsg ) {
 			FlashMessage::add( FlashMessage::TYPE_ERROR, $errorMsg );
