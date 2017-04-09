1bebe65966b3e865af6ace074ca1cbc4edf6ba39
Kevin Studer <kevin.studer@id.unibe.ch>
2017-04-09 21:53:37 +0200

    added user and tour import, not fully functional

---
+++ wordpress/wp-content/plugins/bergclub-plugin/Commands/Entities/Adressen.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevstuder
+ * Date: 09.04.17
+ * Time: 21:12
+ */
+
+namespace BergclubPlugin\Commands\Entities;
+
+
+class Adressen
+{
+    public $id;
+    public $firstName;
+    public $lastName;
+    public $salutation;
+    public $number;
+    public $category;
+    public $street;
+    public $plz;
+    public $place;
+    public $phonePrivate;
+    public $phoneBusiness;
+    public $phoneMobile;
+    public $email;
+    public $birthday;
+    public $ahv;
+}
