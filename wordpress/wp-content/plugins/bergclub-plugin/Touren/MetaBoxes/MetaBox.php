13266706ea4cf9320884b37e239bd07dc2282002
Kevin Studer <kevin.studer@id.unibe.ch>
2017-04-29 13:13:55 +0200

    save data even when title is empty

--- wordpress/wp-content/plugins/bergclub-plugin/Touren/MetaBoxes/MetaBox.php
+++ wordpress/wp-content/plugins/bergclub-plugin/Touren/MetaBoxes/MetaBox.php
@@ -21,7 +21,7 @@ abstract class MetaBox
     {
         self::$registeredBoxes[] = $this;
         //ensure that action hook only gets registered once
-        if (!self::$saveActionRegistered){
+        if (!self::$saveActionRegistered) {
             self::$saveActionRegistered = true;
             //we create a hook only for this specific post type
             if (function_exists('\add_action')) {
@@ -65,7 +65,8 @@ abstract class MetaBox
      * @param $posttype
      * @return bool
      */
-    public function isValid($values, $posttype) {
+    public function isValid($values, $posttype)
+    {
         return true;
     }
 
@@ -74,7 +75,8 @@ abstract class MetaBox
      *
      * @return array
      */
-    protected function addAdditionalValuesForView() {
+    protected function addAdditionalValuesForView()
+    {
         return array();
     }
 
@@ -85,13 +87,14 @@ abstract class MetaBox
      * @param $values array of transmitted values
      * @return array
      */
-    protected function preSave($values) {
+    protected function preSave($values)
+    {
         return $values;
     }
 
     public function add()
     {
-        $screens = [ BCB_CUSTOM_POST_TYPE_TOUREN ];
+        $screens = [BCB_CUSTOM_POST_TYPE_TOUREN];
         foreach ($screens as $screen) {
             \add_meta_box(
                 $this->getUniqueMetaBoxName(),
@@ -108,7 +111,7 @@ abstract class MetaBox
         //ensure that is not a revision (seems to have nothing to do with the state "Review")
         //ensure that it is not autosave which is calling
         //ensure that action is set and the action is 'editpost'
-        if( ! ( wp_is_post_revision( $postId) || wp_is_post_autosave( $postId ) ) && isset($_POST['action']) && $_POST['action'] == 'editpost') {
+        if (!(wp_is_post_revision($postId) || wp_is_post_autosave($postId)) && isset($_POST['action']) && $_POST['action'] == 'editpost') {
             //workaround (otherwise the method still gets called multiple times
             if (!self::$alreadyValidated) {
                 self::$alreadyValidated = true;
@@ -124,37 +127,37 @@ abstract class MetaBox
                 $title = get_the_title($postId);
                 $originalStatus = $_POST['original_post_status'];
 
-                if($originalStatus == "auto-draft"){
+                if ($originalStatus == "auto-draft") {
                     $originalStatus = "draft";
                 }
 
                 $valid = true;
-                if($title == ""){
+                if ($title == "") {
                     $valid = false;
                     FlashMessage::add(FlashMessage::TYPE_ERROR, "Bitte geben sie einen Titel für diese Tour an.");
-                } else {
-                    foreach (self::$registeredBoxes as $box) {
-                        $filteredValues = $this->preSave($_POST);
-                        /* @var MetaBox $box */
-                        foreach ($box->getUniqueFieldNames() as $fieldId) {
-                            if (array_key_exists($fieldId, $filteredValues)) {
-                                \update_post_meta(
-                                    $postId,
-                                    $fieldId,
-                                    $filteredValues[$fieldId]
-                                );
-                            }
-                        }
-                        //we don't want to validate a freshly created post (status: 'auto-draft')
-                        if ($status != 'auto-draft') {
-                            //IRGENDWO HIER MÜSST IHR EINE LÖSUNG FINDEN UM GEWISSE FELDER NUR ZU VALIDIEREN, WENN $status = 'pending' ODER 'publish'
-                            if (!$box->isValid($filteredValues, $status) ) {
-                                $valid = false;
-                            }
+                }
+
+                foreach (self::$registeredBoxes as $box) {
+                    $filteredValues = $this->preSave($_POST);
+                    /* @var MetaBox $box */
+                    foreach ($box->getUniqueFieldNames() as $fieldId) {
+                        if (array_key_exists($fieldId, $filteredValues)) {
+                            \update_post_meta(
+                                $postId,
+                                $fieldId,
+                                $filteredValues[$fieldId]
+                            );
                         }
                     }
-
+                    //we don't want to validate a freshly created post (status: 'auto-draft')
+                    if ($status != 'auto-draft') {
+                        //IRGENDWO HIER MÜSST IHR EINE LÖSUNG FINDEN UM GEWISSE FELDER NUR ZU VALIDIEREN, WENN $status = 'pending' ODER 'publish'
+                        if (!$box->isValid($filteredValues, $status)) {
+                            $valid = false;
+                        }
+                    }
                 }
+
 
                 if (!$valid) {
                     file_put_contents($postId . "_" . uniqid(), "");
@@ -178,49 +181,54 @@ abstract class MetaBox
             // re-hook this function again
             add_action('save_post_' . BCB_CUSTOM_POST_TYPE_TOUREN, [$this, 'save']);
         }
+}
+
+public
+function redirectPostLocation($location, $postId)
+{
+    $status = get_post_status($postId);
+
+    //remove any system message (otherwise it could be e.g. 'successful published', but new state is 'draft')
+    $location = remove_query_arg('message', $location);
+
+    //forward to touren overview when publish button was clicked and state really is 'publish'.
+    if (isset($_POST['publish']) && $status == 'publish') {
+        $location = admin_url("edit.php?post_type=" . BCB_CUSTOM_POST_TYPE_TOUREN);
     }
 
-    public function redirectPostLocation($location, $postId){
-        $status = get_post_status($postId);
+    return $location;
+}
 
-        //remove any system message (otherwise it could be e.g. 'successful published', but new state is 'draft')
-        $location = remove_query_arg('message', $location);
-
-        //forward to touren overview when publish button was clicked and state really is 'publish'.
-        if(isset($_POST['publish']) && $status == 'publish'){
-            $location = admin_url( "edit.php?post_type=" . BCB_CUSTOM_POST_TYPE_TOUREN );
-        }
-
-        return $location;
+public
+function html($post)
+{
+    $values = array();
+    foreach ($this->getUniqueFieldNames() as $fieldId) {
+        $values[$fieldId] = get_post_meta($post->ID, $fieldId, true);
     }
 
-    public function html($post)
-    {
-        $values = array();
-        foreach ($this->getUniqueFieldNames() as $fieldId) {
-            $values[$fieldId] = get_post_meta($post->ID, $fieldId, true);
-        }
-
-        if(!file_exists(__DIR__ . '/cache')){
-            mkdir(__DIR__ . '/cache');
-        }
-        $arguments = array_merge(array('values' => $values), $this->addAdditionalValuesForView());
-        $blade = new BladeInstance(__DIR__ . '/../views', __DIR__ . '/../cache');
-        echo $blade->render(
-            $this->getViewName(),
-            $arguments
-        );
-
+    if (!file_exists(__DIR__ . '/cache')) {
+        mkdir(__DIR__ . '/cache');
     }
+    $arguments = array_merge(array('values' => $values), $this->addAdditionalValuesForView());
+    $blade = new BladeInstance(__DIR__ . '/../views', __DIR__ . '/../cache');
+    echo $blade->render(
+        $this->getViewName(),
+        $arguments
+    );
 
-    /**
-     * Returns true if its a valid time
-     * @param $test
-	 * @return bool
-     */
-    public function isValidTime( $test ): bool {
-        $match_duration_format = preg_match( "/^([01]?[0-9]|2[0-3])\:+[0-5][0-9]$/", $test ) === 1;
+}
 
-        return $match_duration_format;
-    }
+/**
+ * Returns true if its a valid time
+ * @param $test
+ * @return bool
+ */
+public
+function isValidTime($test): bool
+{
+    $match_duration_format = preg_match("/^([01]?[0-9]|2[0-3])\:+[0-5][0-9]$/", $test) === 1;
+
+    return $match_duration_format;
+}
 }
