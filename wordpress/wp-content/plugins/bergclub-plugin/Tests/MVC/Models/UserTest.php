7dda4ba10a2fa0e078c67a581d013ca3c63e7652
Kevin Studer <kevin.studer@id.unibe.ch>
2017-05-07 11:43:47 +0200

    fix constant getter when invariant is equal to zero

--- wordpress/wp-content/plugins/bergclub-plugin/Tests/MVC/Models/UserTest.php
+++ wordpress/wp-content/plugins/bergclub-plugin/Tests/MVC/Models/UserTest.php
@@ -5,3 +5,40 @@
  * Date: 07.05.17
  * Time: 11:30
  */
+
+namespace BergclubPlugin\Tests\MVC\Models;
+
+use PHPUnit\Framework\TestCase;
+use BergclubPlugin\MVC\Models\User;
+
+class UserTest extends TestCase
+{
+    /**
+     * @var User
+     */
+    private $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->user = new User();
+    }
+
+    /**
+     * @test
+     */
+    public function programShipmentOneEqualsToYes()
+    {
+        $this->user->program_shipment = '1';
+        $this->assertEquals('Ja', $this->user->program_shipment);
+    }
+
+    /**
+     * @test
+     */
+    public function programShipmentZeroEqualsToNo()
+    {
+        $this->user->program_shipment = '0';
+        $this->assertEquals('Nein', $this->user->program_shipment);
+    }
+}
