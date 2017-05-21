1513a7ca0ddc3b8d409c565a455343c614d4da19
Mathias Stocker <mathias.stocker@students.unibe.ch>
2017-05-21 02:59:01 +0200

    Update 'Schwierigkeitsgrade' according to customer

--- wordpress/wp-content/plugins/bergclub-plugin/Stammdaten/activate.php
+++ wordpress/wp-content/plugins/bergclub-plugin/Stammdaten/activate.php
@@ -26,51 +26,85 @@ Option::set('mitgliederbeitraege', $mitgliederBeitraege);
  *  create the default "Tourenarten"
  */
 $tourenarten = [
+    'bcb_abendwanderung' => 'Abendwanderung',
     'bcb_bergtour' => 'Bergtour',
-    'bcb_skitour' => 'Skitour',
-    'bcb_langlauf' => 'Langlauf',
-    'bcb_klettertraining' => 'Klettertraining',
-    'bcb_velotour' => 'Velotour',
     'bcb_hochtour' => 'Hochtour',
+    'bcb_hoehlentour' => 'Höhlentour',
+    'bcb_klettersteig' => 'Klettersteig',
+    'bcb_klettertour' => 'Klettertour',
+    'bcb_klettertraining' => 'Klettertraining',
+    'bcb_langlauf' => 'Langlauf',
     'bcb_pistenfahren' => 'Pistenfahren',
     'bcb_schneeschuhwanderung' => 'Schneeschuhw.',
-    'bcb_klettertour' => 'Klettertour',
-    'bcb_diverses' => 'Diverses',
+    'bcb_skitour' => 'Skitour',
+    'bcb_velotour' => 'Velotour',
     'bcb_wanderung' => 'Wanderung',
-    'bcb_klettersteig' => 'Klettersteig',
-    'bcb_hoehlentour' => 'Höhlentour',
-    'bcb_abendwanderung' => 'Abendwanderung',
+    'bcb_diverses' => 'Diverses',
 ];
 
 /**
  *  create the default "Schwierigkeiten" for the "Tourenarten"
  */
 $schwierigkeiten = [
-    '',
-    'T1 (Wanderung)',
-    'T2 (Bergwandern)',
-    'T3 (Ansp. Bergwandern)',
-    'T4 (Alpinwanderung)',
-    'T5 (Ansp. Alpinwandern)',
-    'T6 (Schw. Alpinwandern)',
-    'K1 (sehr einfach)',
-    'K2 (einfach)',
-    'K3 (mässig schwierig)',
-    'K4 (schwierig)',
-    'K5 (sehr schwierig)',
-    'K6 (äusserst schwierig)',
-    'L (leicht)',
-    'WS (wenig schwierig)',
-    'ZS (ziemlich schwierig)',
-    'S (schwierig)',
+    'bcb_abendwanderung' => [],
+    'bcb_bergtour' => [
+        'T2 - Bergwandern',
+        'T3 - anspruchsv. Bergwandern',
+        'T4 - Alpinwandern',
+        'T5 - anspruchsv. Alpinwandern',
+        'T6 - schwieriges Alpinwandern',
+    ],
+    'bcb_hochtour' => [
+        'L - leicht',
+        'WS - wenig schwierig',
+        'ZS - ziemlich schwierig',
+        'S - schwierig',
+        'SS - sehr schwierig',
+        'AS - ausserord. schwierig',
+        'EX - extrem schwierig',
+    ],
+    'bcb_hoehlentour' => [],
+    'bcb_klettersteig' => [],
+    'bcb_klettertour' => [
+        'I - Geringe Schw.',
+        'II - Mässige Schw.',
+        'III - Mittlere Schw.',
+        'IV - Grosse Schw.',
+        'V - Sehr grosse Schw.',
+        'VI - Überaus grosse Schw.',
+        'VII - Aussergew. Schw.',
+    ],
+    'bcb_klettertraining' => [],
+    'bcb_langlauf' => [],
+    'bcb_pistenfahren' => [],
+    'bcb_schneeschuhwanderung' => [
+        'WT 1 - Leicht',
+        'WT 2 - Normal',
+        'WT 3 - Anspruchsvoll',
+        'WT 4 - Schwierig',
+        'WT 5 - Alpine Tour',
+        'WT 6 - Anspruchsv. Alpine Tour',
+    ],
+    'bcb_skitour' => [
+        'L - leicht',
+        'WS - wenig schwierig',
+        'ZS - ziemlich schwierig',
+        'S - schwierig',
+        'SS - sehr schwierig',
+        'AS - ausserord. schwierig',
+        'EX - extrem schwierig',
+    ],
+    'bcb_velotour' => [],
+    'bcb_wanderung' => [],
+    'bcb_diverses' => [],
 ];
 
 /**
  *  add the "Tourenarten" to WP option table and add each "Schwierigkeit"
- *  to each "Tourenart" as default
+ *  to each "Tourenart"
  */
 Option::set('tourenarten', $tourenarten);
 
-foreach ($tourenarten as $key => $tourenart) {
-    Option::set($key, $schwierigkeiten);
+foreach ($schwierigkeiten as $key => $arr) {
+    Option::set($key, $arr);
 }
